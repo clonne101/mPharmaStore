eeff920235ef143f0c1b07c090e1611f61de8e87
Jeffery Osei <jeff@jefferyclonne.com>
2021-05-17 05:27:09 +0000

    load products

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,6 +1,9 @@
 import { useLocalStore, action, actionOn } from "easy-peasy";
 import swal from "sweetalert";
-import { ShoppingCartIcon } from "@heroicons/react/outline";
+import { useEffect } from "react";
+import superagent from "superagent";
+import _ from "lodash";
+import { PlusIcon } from "@heroicons/react/outline";
 
 const noty = {
   closeOnClickOutside: true,
@@ -12,83 +15,168 @@ const noty = {
   timer: 3000,
 };
 
+const GetLatestPrice = (prices, returnLatest = false) => {
+  const price = _.orderBy(prices, ["date"], ["desc"]);
+
+  if (returnLatest) return price[0];
+
+  return price;
+};
+
 const App = () => {
   const [_state, _actions] = useLocalStore(() => ({
-    items: [],
-    add: action((state, obj) => {
-      state.items.push(obj);
+    products: [],
+    setInitial: action((state, data) => {
+      state.products = data;
+    }),
+    add: action((state, product) => {
+      state.products.push(product);
     }),
     remove: action((state, id = null) => {
       if (id) {
-        state.items.splice(id, 1);
+        state.products.splice(id, 1);
       } else {
-        state.items = [];
+        state.products = [];
       }
     }),
-    onAddToCart: actionOn(
+    onAdd: actionOn(
       (actions) => actions.add,
       (state, target) => {
         swal({
           ...noty,
           title: "Success",
-          text: "Item added to cart",
+          text: "Item added to product list",
           icon: "success",
         });
       }
     ),
-    onRemoveFromCart: actionOn(
+    onRemove: actionOn(
       (actions) => actions.remove,
       (state, target) => {
         swal({
           ...noty,
           title: "Removed",
-          text: "Item removed from cart",
+          text: "Item removed from product list",
           icon: "error",
         });
       }
     ),
   }));
 
-  // return (
-  //   <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4'>
-  //     <div className='flex-shrink-0'>
-  //       <img className='h-12 w-12' src={logo} alt='ChitChat Logo' />
-  //     </div>
-  //     <div>
-  //       <div className='text-xl font-medium text-black'>ChitChat</div>
-  //       <p className='text-gray-500'>{_state.items.length}</p>
-  //       <button onClick={() => _actions.add("hello")}>add to cart</button>
-  //       <br />
-  //       {_state.items.length > 0 && (
-  //         <button onClick={() => _actions.remove()}>remove from cart</button>
-  //       )}
-  //     </div>
-  //   </div>
-  // );
+  useEffect(() => {
+    async function fetchData() {
+      const res = await superagent.get(
+        "http://www.mocky.io/v2/5c3e15e63500006e003e9795"
+      );
+      _actions.setInitial(res.body.products);
+    }
+    fetchData();
+  }, []);
 
   return (
-    <div className='max-w-7xl mx-auto px-4 sm:px-6'>
-      <div className='flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10'>
-        <div className='flex justify-start lg:w-0 lg:flex-1'>
-          <a href='#'>
-            <span className='sr-only'>Workflow</span>
-            <img
-              className='h-8 w-auto sm:h-10'
-              src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
-              alt=''
-            />
-          </a>
-        </div>
-        <div className='hidden md:flex items-center justify-end md:flex-1 lg:w-0'>
-          <button className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700'>
-            <ShoppingCartIcon className={"h-6 w-6"} />{" "}
-            <span className='rounded-full h-5 w-5 flex items-center justify-center bg-white text-black'>
-              {_state.items.length}
-            </span>
-          </button>
+    <>
+      {/* Render navigation */}
+      <div className='max-w-7xl mx-auto px-4 sm:px-6'>
+        <div className='flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10'>
+          <div className='flex justify-start lg:w-0 lg:flex-1'>
+            <a href='/'>
+              <span className='sr-only'>Workflow</span>
+              <img
+                className='h-8 w-auto sm:h-10'
+                src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
+                alt=''
+              />
+            </a>
+          </div>
+          <div className='hidden md:flex items-center justify-end md:flex-1 lg:w-0'>
+            <button
+              onClick={() => _actions.add("hello")}
+              className='ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700'
+            >
+              <span className='flex items-center justify-center text-white'>
+                Add New Product
+              </span>
+              <PlusIcon className={"h-6 w-6"} />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      {/* Render product table */}
+      {_state.products.length === 0 ? (
+        <div>Loading...</div>
+      ) : (
+        <div className='flex flex-col'>
+          <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+            <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
+              <div className='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
+                <table className='min-w-full divide-y divide-gray-200'>
+                  <thead className='bg-gray-50'>
+                    <tr>
+                      <th
+                        scope='col'
+                        className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                      >
+                        Name
+                      </th>
+                      <th
+                        scope='col'
+                        className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                      >
+                        Price (GHS)
+                      </th>
+                      <th scope='col' className='relative px-6 py-3'>
+                        <span className='sr-only'>View Prices</span>
+                      </th>
+                      <th scope='col' className='relative px-6 py-3'>
+                        <span className='sr-only'>Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className='bg-white divide-y divide-gray-200'>
+                    {_state.products.map((item) => {
+                      const latestPrice = GetLatestPrice(item.prices, true);
+                      return (
+                        <tr key={item.id}>
+                          <td className='px-6 py-4 whitespace-nowrap'>
+                            <div className='flex items-center'>
+                              <div className='text-sm font-medium text-gray-900'>
+                                {item.name}
+                              </div>
+                            </div>
+                          </td>
+                          <td className='px-6 py-4 whitespace-nowrap'>
+                            <div className='text-sm text-gray-900'>
+                              {latestPrice.price}
+                            </div>
+                          </td>
+                          <td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>
+                            <a
+                              href='#'
+                              className='text-indigo-600 hover:text-indigo-900'
+                            >
+                              View Prices
+                            </a>
+                          </td>
+                          <td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>
+                            <a
+                              href='#'
+                              className='text-indigo-600 hover:text-indigo-900'
+                            >
+                              Edit
+                            </a>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      ;
+    </>
   );
 };
 
