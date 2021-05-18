349ba93a8dca77aeb279bf4a25791d2b83b4fb67
Jeffery Osei <jeff@jefferyclonne.com>
2021-05-18 12:25:19 +0000

    complete test

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # About
 
-An mPharma test store
+An mPharma test store manager
 
 ## Run
+
 - Node 8 or later is required
 - Install Yarn `npm install -g yarn`
 - Change directory into the root of the project folder
 - Run `yarn` or `npm install`
 - Now Run `yarn start` or `npm run start`
 - Now visit `http://localhost:3000/` in your preferred browser
+
+## Packages
+
+- (Data validation) Yup = <https://github.com/jquense/yup>
+- (State manager) Easy-Peasy = <https://easy-peasy.dev/>
+- (CSS framework) TailwindCss = <https://tailwindcss.com/docs/installation>
+- (Helper functions) Lodash = <https://lodash.com/>
+- (Form manager) Formik = <https://formik.org/docs/overview>
+- (API requests) Superagent = <https://github.com/visionmedia/superagent>
+- (Notifications) Sweetalert = <https://sweetalert2.github.io/>
--- src/ui/extensions/ViewPricesModal.js
+++ src/ui/extensions/ViewPricesModal.js
@@ -56,6 +56,52 @@ const ViewPricesModal = ({
                   </div>
                 </div>
                 <div>
+                  {/* Table */}
+                  <div className='flex flex-col'>
+                    <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+                      <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
+                        <div className='overflow-hidden'>
+                          <table className='min-w-full divide-y divide-gray-200'>
+                            <thead className='bg-gray-50'>
+                              <tr>
+                                <th
+                                  scope='col'
+                                  className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                                >
+                                  Amount (GHS)
+                                </th>
+                                <th
+                                  scope='col'
+                                  className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'
+                                >
+                                  Date
+                                </th>
+                              </tr>
+                            </thead>
+                            <tbody className='bg-white divide-y divide-gray-200'>
+                              {product.prices &&
+                                product.prices.length > 0 &&
+                                product.prices.map((item, index) => (
+                                  <tr key={index}>
+                                    <td className='px-6 py-4 whitespace-nowrap'>
+                                      <div className='text-sm text-gray-900'>
+                                        {item.price}
+                                      </div>
+                                    </td>
+                                    <td className='px-6 py-4 whitespace-nowrap'>
+                                      <div className='text-sm text-gray-900'>
+                                        {new Date(item.date).toUTCString()}
+                                      </div>
+                                    </td>
+                                  </tr>
+                                ))}
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Buttons */}
                   <div className='px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse'>
                     <button
                       type='button'
